98e9d0fdd754bac6ec39fb8105e76223d0c74c90
Lasse Hüffler <Lasse@hueffler.de>
2022-09-05 22:34:10 +0200

    big deletion

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/dev/Thesis Vk/tests
-# Build directory: D:/dev/Thesis Vk/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Test1 "test1")
-set_tests_properties(Test1 PROPERTIES  _BACKTRACE_TRIPLES "D:/dev/Thesis Vk/tests/CMakeLists.txt;30;add_test;D:/dev/Thesis Vk/tests/CMakeLists.txt;0;")
